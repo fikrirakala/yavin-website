0c314cda4e154b469d3b90704d9a315455de9d12
Fikri Rakala <fikrirakala@gmail.com>
2023-07-01 06:23:15 +0800

    create to top button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 // Navbar
 function userScroll() {
   const navbar = document.querySelector(".navbar");
+  const toTopBtn = document.querySelector("#to-top");
 
   window.addEventListener("scroll", function () {
     if (this.scrollY > 100) {
       navbar.classList.add("navbar-sticky");
+      toTopBtn.classList.add("show");
     } else {
       navbar.classList.remove("navbar-sticky");
+      toTopBtn.classList.remove("show");
     }
   });
+
+  toTopBtn.addEventListener("click", scrollToTop);
+}
+
+// Scroll to top
+function scrollToTop() {
+  document.documentElement.scrollTop = 0;
 }
 
 document.addEventListener("DOMContentLoaded", userScroll);
